a75f8f2b60a410d35bbbca6f5181c33f18e484ec
Tanuj1718 <125687187+Tanuj1718@users.noreply.github.com>
2024-10-02 00:08:20 +0530

    cors issue solved

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,26 +1,28 @@
-import express from "express"
-const app = express()
-import dotenv from "dotenv"
-dotenv.config({
-    path: "./.env"
-})
+import express from 'express';
+import dotenv from 'dotenv';
 
+dotenv.config({ path: './.env' });
 
+const app = express();
+
+// Set up CORS headers
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://engage-learning.vercel.app/');
-    res.header('Access-Control-Allow-Headers', 'Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Accept-Language', 'Content-Language');
-    res.header('Access-Control-Allow-Methods', 'GET', 'DELETE', 'POST', 'HEAD', 'OPTIONS')
-    next();
-  });
-//common middlewares
-app.use(express.json())
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Accept-Language, Content-Language');
+  res.header('Access-Control-Allow-Methods', 'GET, DELETE, POST, HEAD, OPTIONS');
+  next();
+});
 
+// Common middlewares
+app.use(express.json());
 
-//import routes
-import signupRouter from "./routes/signup.route.js"
-import signinRouter from "./routes/signin.route.js"
-import formRouter from "./routes/form.route.js"
-app.use('/register', signupRouter)
-app.use('/login', signinRouter)
-app.use('/form', formRouter)
-export {app}
+// Import routes
+import signupRouter from './routes/signup.route.js';
+import signinRouter from './routes/signin.route.js';
+import formRouter from './routes/form.route.js';
+
+app.use('/register', signupRouter);
+app.use('/login', signinRouter);
+app.use('/form', formRouter);
+
+export { app };
