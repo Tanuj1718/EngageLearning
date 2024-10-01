fee713374ace4a7c502948f544a4ad239ed509c8
Tanuj1718 <125687187+Tanuj1718@users.noreply.github.com>
2024-10-01 18:25:14 +0530

    solved cors issue

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,12 +8,12 @@ dotenv.config({
 
 
 
-app.use(cors())
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-  });
+app.use(cors());
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//     next();
+//   });
 
 //common middlewares
 app.use(express.json())
