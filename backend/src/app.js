d669328df5d955c199463aafb1b34cfd1ca06eb3
Tanuj1718 <125687187+Tanuj1718@users.noreply.github.com>
2024-10-01 14:57:02 +0530

    solved cors issue

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,7 @@ dotenv.config({
 
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Origin', 'https://engage-learning.vercel.app/');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
   });
