a763530c28acda1f9973cf57aaed8ede4d1092ae
Tanuj1718 <125687187+Tanuj1718@users.noreply.github.com>
2024-10-01 19:39:48 +0530

    cors bugs solved

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,26 +1,29 @@
-import express from "express";
-import cors from 'cors';
-import dotenv from "dotenv";
-
+import express from "express"
+import cors from "cors"
+const app = express()
+import dotenv from "dotenv"
 dotenv.config({
     path: "./.env"
-});
+})
 
-const app = express();
 
-// Enable CORS
-app.use(cors())
 
-// Common middleware
-app.use(express.json());
 
-// Import routes
-import signupRouter from "./routes/signup.route.js";
-import signinRouter from "./routes/signin.route.js";
-import formRouter from "./routes/form.route.js";
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Accept-Language, Content-Language');
+    res.header('Access-Control-Allow-Methods', 'GET, DELETE, POST, HEAD, OPTIONS')
+    next();
+  });
+//common middlewares
+app.use(express.json())
 
-app.use('/register', signupRouter);
-app.use('/login', signinRouter);
-app.use('/form', formRouter);
 
-export { app };
+//import routes
+import signupRouter from "./routes/signup.route.js"
+import signinRouter from "./routes/signin.route.js"
+import formRouter from "./routes/form.route.js"
+app.use('/register', signupRouter)
+app.use('/login', signinRouter)
+app.use('/form', formRouter)
+export {app}
