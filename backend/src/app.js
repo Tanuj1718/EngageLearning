84bf047a3d7134ac28b59f63d544f9a6aa79e4fb
Tanuj1718 <125687187+Tanuj1718@users.noreply.github.com>
2024-10-02 00:02:21 +0530

    url changes

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,7 @@ dotenv.config({
 
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://engage-learning.vercel.app');
+    res.header('Access-Control-Allow-Origin', 'https://engage-learning.vercel.app/');
     res.header('Access-Control-Allow-Headers', 'Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Accept-Language', 'Content-Language');
     res.header('Access-Control-Allow-Methods', 'GET', 'DELETE', 'POST', 'HEAD', 'OPTIONS')
     next();
