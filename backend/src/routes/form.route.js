4d26041ee0369545e67e5abda7ad46059993ebbd
Tanuj1718 <125687187+Tanuj1718@users.noreply.github.com>
2024-10-02 11:41:33 +0530

    updated cors issue

--- backend/src/routes/form.route.js
+++ backend/src/routes/form.route.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { createResponse } from "../controllers/form.controller.js";
 
+
 const router = Router()
 router.route('/ideas').post(createResponse)
 export default router
--- backend/src/routes/signup.route.js
+++ backend/src/routes/signup.route.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import {Signup} from "../controllers/auth.controller.js";
+import cors from 'cors'
 
 const router = Router()
+router.use(cors())
 router.route('/signup').post(Signup)
 export default router
