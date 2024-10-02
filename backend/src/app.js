69141157751c3fc5e49d8326c0c6543a3d68f5a8
Tanuj1718 <125687187+Tanuj1718@users.noreply.github.com>
2024-10-02 09:59:36 +0530

    solved cors issue

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,25 +1,16 @@
 import express from 'express';
 import dotenv from 'dotenv';
-
 dotenv.config({ path: './.env' });
-
+import cors from 'cors';
 const app = express();
 
 // Set up CORS headers
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Accept-Language, Content-Language');
-  res.header('Access-Control-Allow-Methods', 'GET, DELETE, POST, HEAD, OPTIONS');
-
-  if(req.method === 'OPTIONS'){
-    return res.status(200).json({})
-  }
-  next();
-});
+app.use(cors({
+  origin: 'https://engage-learning.vercel.app'
+}))
 
 // Common middlewares
 app.use(express.json());
-
 // Import routes
 import signupRouter from './routes/signup.route.js';
 import signinRouter from './routes/signin.route.js';
@@ -28,5 +19,4 @@ import formRouter from './routes/form.route.js';
 app.use('/register', signupRouter);
 app.use('/login', signinRouter);
 app.use('/form', formRouter);
-
 export { app };
